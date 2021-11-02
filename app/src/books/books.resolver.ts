8174ee8b9cdcdfc4e68fb6e879e11356a2638fc5
yuki <yuki.onishi1129@gmail.com>
2021-11-03 01:09:44 +0900

    add: subscription

--- app/src/books/books.resolver.ts
+++ app/src/books/books.resolver.ts
@@ -1,11 +1,21 @@
 import { NotFoundException } from '@nestjs/common';
-import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+  Subscription,
+} from '@nestjs/graphql';
+import { PubSub } from 'graphql-subscriptions';
 /* services */
 import { BooksService } from './books.service';
 /* entities */
 import { Book } from './entities/book.entity';
 /* dto */
 import { NewBookInput } from './dto/newBook.input';
+
+const pubSub = new PubSub();
 
 // @Resolver: リゾルバを示すdecorator
 // 引数にリゾルバに対応するmodelのクラスを定義する必要あり
@@ -32,12 +42,19 @@ export class BooksResolver {
   }
 
   @Mutation(() => Book)
-  addBook(@Args('newBook') newBook: NewBookInput): Promise<Book> {
-    return this.booksService.create(newBook);
+  async addBook(@Args('newBook') newBook: NewBookInput): Promise<Book> {
+    const createdBook = await this.booksService.create(newBook);
+    pubSub.publish('bookCreated', { bookCreated: createdBook });
+    return createdBook;
   }
 
   @Mutation(() => Boolean)
   async removeBook(@Args({ name: 'id', type: () => Int }) id: number) {
     return this.booksService.remove(id);
+  }
+
+  @Subscription(() => Book)
+  bookCreated() {
+    return pubSub.asyncIterator('bookCreated');
   }
 }
